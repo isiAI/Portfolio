536e3f96a56e756382b54167ede575ae2bada750
Bjoern Isenbuegel <bjoern.isenbuegel@kanzlei-job.de>
2023-09-10 16:32:01 +0200

    personalised this nice minimal portfolio site

--- js/demo.js
+++ js/demo.js
@@ -7,7 +7,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
     dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    lineColor: '#5cbdff'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
